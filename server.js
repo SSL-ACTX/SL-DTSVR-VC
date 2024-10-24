e9e7595827c85ef13117863c94fe28a70e92c03e
Seuriin <seuriin@gmail.com>
2024-10-25 00:51:20 +0800

    Rate Limit

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 #!/usr/bin/env node
 'use strict'
 const app = require('express')()
+const rateLimit = require('express-rate-limit') 
 const authenticate = require('./src/authenticate')
 const params = require('./src/params')
 const proxy = require('./src/proxy')
 
 const PORT = process.env.PORT || 8080
+
+// Rate limiter max 100 requests per 15 minutes
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100, 
+  message: 'Too many requests from this IP, please try again after 15 minutes'
+})
+
+// Apply rate limiting middleware globally
+app.use(limiter)
 
 app.enable('trust proxy')
 app.get('/', authenticate, params, proxy)
